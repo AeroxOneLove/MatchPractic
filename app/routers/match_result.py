616a7197ab757d498f0f3be3742be56bfd13fbe7
AeroxOneLove <saipullaev.f@mail.ru>
2025-03-27 22:17:03 +0300

    uuid поправил на str и ненужный перевод его в строку, процент совпадения ьеперь float, убрал константу (match_results_store)

--- app/routers/match_result.py
+++ app/routers/match_result.py
@@ -3,11 +3,9 @@ from app.schemas import MatchResult
 from app.services import match_results_store
 
 
-router = APIRouter()
-
 
 router = APIRouter()
 
 @router.get("/match_results", response_model=list[MatchResult])
-async def get_match_results():
+async def get_match_results() -> list[MatchResult]:
     return match_results_store  
