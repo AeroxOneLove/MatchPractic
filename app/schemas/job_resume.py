3c8bbcbee9ab02d34393eea88cdc7419273b8b49
AeroxOneLove <saipullaev.f@mail.ru>
2025-03-30 00:22:51 +0300

    поправлены схемы

--- app/schemas/job_resume.py
+++ app/schemas/job_resume.py
@@ -2,12 +2,19 @@ from datetime import date
 from typing import Optional
 from pydantic import BaseModel, UUID4
 
-    
-class JobResume(BaseModel):
-    start_date: date
-    end_date: Optional[date] = None
+class ExperienceResponse(BaseModel):
     uuid: str
+    company_name: str
     position: str
-    skills: list[str]
-    about_me: str
+    start_date: date    
+    end_date: Optional[date] = None
+
+
+class JobResume(BaseModel):
+    uuid: str
+    full_name: str 
+    position: str 
+    skills: list[str] 
+    experiences: list[ExperienceResponse]
+    about_me: str 
 
